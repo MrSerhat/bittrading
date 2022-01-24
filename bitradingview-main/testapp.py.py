89b39f13fe328b29e8ece8e99a93ea8202526459
Serhat ASLAN <head_hunter_gs@hotmail.com>
2022-01-24 21:59:53 +0300

    Add files via upload

---
+++ bitradingview-main/testapp.py.py
@@ -0,0 +1,33 @@
+import json, config
+from flask import Flask, request, jsonify, render_template
+from binance.client import Client
+from binance.enums import *
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    #print(request.data)
+    data = json.loads(request.data)
+    
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
+
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side, quantity, "DOGEUSD")
+
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
